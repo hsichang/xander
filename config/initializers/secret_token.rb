bd53dd9aec4031c890172de8089c0f442d0d2a5f
hsichang <hsi.chang@gmail.com>
2014-06-06 15:30:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xander::Application.config.secret_token = '89c83decc48ed07e9e76a88b6e2753f1d4b25e4df979e423671dd3f7712841bddeae45b5a88a7cb62df73572bb98543b263128a54f68823bc043758abedd0a26'
